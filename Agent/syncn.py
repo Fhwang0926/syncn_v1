618eabe5a321d39568738dc7aaed9b536877d170
FHWANG\root <hdh0926@naver.com>
2018-09-22 11:16:39 +0900

    ui 확정

--- Agent/syncn.py
+++ Agent/syncn.py
@@ -1,173 +1,181 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'main.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost!
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+# auth : bluehdh0926@gmail.com
+# comment format like html
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QMessageBox, QMainWindow
+import sys
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.setWindowModality(QtCore.Qt.NonModal)
-        MainWindow.resize(390, 590)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
-        MainWindow.setSizePolicy(sizePolicy)
-        MainWindow.setMinimumSize(QtCore.QSize(390, 590))
-        MainWindow.setMaximumSize(QtCore.QSize(390, 590))
-        MainWindow.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
-        MainWindow.setAcceptDrops(False)
-        MainWindow.setWindowTitle("SyncN")
+class UI(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setObjectName("MainWindow")
+        
+        # icon
         icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap("UI\images\sync.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        MainWindow.setWindowIcon(icon)
-        MainWindow.setWindowOpacity(1.0)
-        MainWindow.setToolTip("")
-        MainWindow.setStatusTip("")
-        MainWindow.setAutoFillBackground(False)
-        MainWindow.setStyleSheet("background-color:rgb(255, 255, 127);")
-        MainWindow.setToolButtonStyle(QtCore.Qt.ToolButtonIconOnly)
-        MainWindow.setAnimated(True)
-        MainWindow.setDocumentMode(True)
-        MainWindow.setDockOptions(QtWidgets.QMainWindow.AllowTabbedDocks|QtWidgets.QMainWindow.AnimatedDocks)
-        MainWindow.setUnifiedTitleAndToolBarOnMac(True)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setObjectName("centralwidget")
-        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.centralwidget)
-        self.verticalLayout_3.setObjectName("verticalLayout_3")
-        self.verticalLayout_2 = QtWidgets.QVBoxLayout()
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.horizontalLayout = QtWidgets.QHBoxLayout()
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem)
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        font = QtGui.QFont()
-        font.setFamily("Bahnschrift Condensed")
-        font.setPointSize(52)
-        font.setBold(True)
-        font.setUnderline(False)
-        font.setWeight(75)
-        font.setStrikeOut(False)
-        self.label_2.setFont(font)
-        self.label_2.setToolTip("")
-        self.label_2.setStatusTip("")
-        self.label_2.setWhatsThis("")
-        self.label_2.setAccessibleName("")
-        self.label_2.setAccessibleDescription("")
-        self.label_2.setStyleSheet("color:rgb(66, 54, 48)")
-        self.label_2.setObjectName("label_2")
-        self.horizontalLayout.addWidget(self.label_2)
-        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout.addItem(spacerItem1)
-        self.verticalLayout_2.addLayout(self.horizontalLayout)
-        self.horizontalLayout_3 = QtWidgets.QHBoxLayout()
-        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout_3.addItem(spacerItem2)
-        self.verticalLayout = QtWidgets.QVBoxLayout()
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.label = QtWidgets.QLabel(self.centralwidget)
-        font = QtGui.QFont()
-        font.setFamily("Bahnschrift SemiBold")
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label.setFont(font)
-        self.label.setFrameShape(QtWidgets.QFrame.NoFrame)
-        self.label.setTextFormat(QtCore.Qt.PlainText)
-        self.label.setScaledContents(False)
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        self.label.setObjectName("label")
-        self.verticalLayout.addWidget(self.label)
-        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
-        self.lineEdit.setSizePolicy(sizePolicy)
-        self.lineEdit.setMinimumSize(QtCore.QSize(280, 45))
-        self.lineEdit.setMaximumSize(QtCore.QSize(280, 45))
-        self.lineEdit.setBaseSize(QtCore.QSize(280, 45))
-        font = QtGui.QFont()
-        font.setFamily("Bahnschrift Condensed")
-        font.setPointSize(14)
-        self.lineEdit.setFont(font)
-        self.lineEdit.setStyleSheet("background-color:rgb(255, 255, 255);\n"
-            "border-style:solid;\n"
-            "border-color:#e5d32e;\n"
-            "border-width:1px;")
-        self.lineEdit.setFrame(True)
-        self.lineEdit.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
-        self.lineEdit.setCursorMoveStyle(QtCore.Qt.LogicalMoveStyle)
-        self.lineEdit.setClearButtonEnabled(True)
-        self.lineEdit.setObjectName("lineEdit")
-        self.verticalLayout.addWidget(self.lineEdit)
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setEnabled(True)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButton.sizePolicy().hasHeightForWidth())
-        self.pushButton.setSizePolicy(sizePolicy)
-        self.pushButton.setMinimumSize(QtCore.QSize(280, 45))
-        self.pushButton.setMaximumSize(QtCore.QSize(280, 45))
-        self.pushButton.setSizeIncrement(QtCore.QSize(0, 0))
-        self.pushButton.setBaseSize(QtCore.QSize(280, 45))
-        font = QtGui.QFont()
-        font.setFamily("Bahnschrift Condensed")
-        font.setPointSize(18)
-        font.setBold(False)
-        font.setWeight(50)
-        self.pushButton.setFont(font)
-        self.pushButton.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
-        self.pushButton.setMouseTracking(False)
-        self.pushButton.setFocusPolicy(QtCore.Qt.ClickFocus)
-        self.pushButton.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
-        self.pushButton.setStatusTip("")
-        self.pushButton.setWhatsThis("")
-        self.pushButton.setAccessibleName("")
-        self.pushButton.setAccessibleDescription("")
-        self.pushButton.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.pushButton.setStyleSheet("background-color:rgb(246, 246, 246);\n"
-            "border-style:solid;\n"
-            "border-color:#e5d32e;\n"
-            "border-width:1px;\n")
-        self.pushButton.setInputMethodHints(QtCore.Qt.ImhNone)
-        self.pushButton.setAutoDefault(False)
-        self.pushButton.setFlat(False)
-        self.pushButton.setObjectName("pushButton")
-        self.verticalLayout.addWidget(self.pushButton)
-        spacerItem3 = QtWidgets.QSpacerItem(20, 60, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
-        self.verticalLayout.addItem(spacerItem3)
-        self.horizontalLayout_3.addLayout(self.verticalLayout)
-        spacerItem4 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout_3.addItem(spacerItem4)
-        self.verticalLayout_2.addLayout(self.horizontalLayout_3)
-        self.verticalLayout_3.addLayout(self.verticalLayout_2)
-        MainWindow.setCentralWidget(self.centralwidget)
+        icon.addPixmap(QtGui.QPixmap("UI/images/sync.ico"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        # window
+        self.setWindowIcon(icon)
+        self.setEnabled(True)
+        self.resize(390, 590)
+        self.setMinimumSize(QtCore.QSize(390, 590))
+        # self.setMaximumSize(QtCore.QSize(390, 590))
+        self.setMouseTracking(True)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setWindowTitle("SyncN")        
+        self.setStyleSheet("background-color:rgb(255, 255, 127);")
+
+        # window - widget
+        self.w_main = QtWidgets.QWidget(self)
+        self.w_main.setStyleSheet("#btn_close:hover { color:black; }\n#btn_close { color:gray; }\n#btn_tray:hover { color:black; }\n#btn_tray { color:gray; }")
+        self.w_main.setObjectName("w_main")
+        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.w_main)
+        self.verticalLayout_3.setObjectName("verticalLayout_3")
+
+        # window - widget - button
+        self.btn_tray = QtWidgets.QPushButton(self.w_main)
+        self.btn_tray.setMinimumSize(QtCore.QSize(30, 30))
+        self.btn_tray.setFont(QtGui.QFont("Corbel", 30, 80))
+        self.btn_tray.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn_tray.setStyleSheet("background-color:rgb(255, 255, 127);\nborder-style:none;\nfont-family:Corbel;\nfont-size:12px;\nfont-weight:900;")
+        self.btn_tray.setObjectName("btn_tray")
+
+        self.btn_close = QtWidgets.QPushButton(self.w_main)
+        self.btn_close.setMinimumSize(QtCore.QSize(30, 30))
+        self.btn_close.setFont(QtGui.QFont("Corbel", 12, 80))
+        self.btn_close.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn_close.setStyleSheet("background-color:rgb(255, 255, 127);\nborder-style:none;")
+        self.btn_close.setObjectName("btn_close")
+        self.btn_close.clicked.connect(self.shutdown)
+
+        self.btn_ok = QtWidgets.QPushButton(self.w_main)
+        self.btn_ok.setEnabled(False)
+        self.btn_ok.setMinimumSize(QtCore.QSize(280, 45))
+        self.btn_ok.setFont(QtGui.QFont("Bahnschrift Condensed", 18, 50))
+        self.btn_ok.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        self.btn_ok.setFocusPolicy(QtCore.Qt.ClickFocus)
+        self.btn_ok.setContextMenuPolicy(QtCore.Qt.NoContextMenu)
+        self.btn_ok.setLayoutDirection(QtCore.Qt.LeftToRight)
+        self.btn_ok.setStyleSheet("background-color:rgb(246, 246, 246);\nborder-style:solid;\nborder-color:#e5d32e;\nborder-width:1px;")
+        self.btn_ok.setInputMethodHints(QtCore.Qt.ImhNone)
+        self.btn_ok.setObjectName("btn_ok")
+
+        # window - widget - label
+        self.l_title = QtWidgets.QLabel(self.w_main)
+        self.l_title.setFont(QtGui.QFont("Bahnschrift Condensed", 52, 80))
+        self.l_title.setStyleSheet("color:rgb(66, 54, 48)")
+        self.l_title.setObjectName("l_title")
+        self.l_info = QtWidgets.QLabel(self.w_main)
+        self.l_info.setFont(QtGui.QFont("Bahnschrift SemiBold", 16, 80))
+        self.l_info.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.l_info.setTextFormat(QtCore.Qt.PlainText)
+        self.l_info.setAlignment(QtCore.Qt.AlignCenter)
+        self.l_info.setObjectName("l_info")
+
+        # window - widget - input
+        self.input_info = QtWidgets.QLineEdit(self.w_main)
+        self.input_info.setMinimumSize(QtCore.QSize(280, 45))
+        self.input_info.setMaximumSize(QtCore.QSize(280, 45))
+        self.input_info.setBaseSize(QtCore.QSize(280, 45))
+        self.input_info.setFont(QtGui.QFont("Bahnschrift Condensed", 14))
+        self.input_info.setStyleSheet("background-color:rgb(255, 255, 255);\nborder-style:solid;\nborder-color:#e5d32e;\nborder-width:1px;")
+        self.input_info.setClearButtonEnabled(True)
+        self.input_info.textChanged.connect(self.checkInput)
+        self.input_info.setObjectName("input_info")
+
+        # window - widget - spacer
+        spacerItem = QtWidgets.QSpacerItem(500, 10, QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Minimum)
+        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        spacerItem2 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        spacerItem3 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+        spacerItem4 = QtWidgets.QSpacerItem(20, 60, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        spacerItem5 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Minimum)
+
+        #layout - header
+        self.la_header = QtWidgets.QHBoxLayout()
+        self.la_header.setSizeConstraint(QtWidgets.QLayout.SetMinimumSize)
+        self.la_header.setSpacing(2)
+        self.la_header.addItem(spacerItem)
+        self.la_header.addWidget(self.btn_tray)
+        self.la_header.addWidget(self.btn_close)
+        self.la_header.setObjectName("la_header")
+        #layout - title
+        self.la_body_all = QtWidgets.QHBoxLayout()
+        self.la_body_all.addItem(spacerItem1)
+        self.la_body_all.addWidget(self.l_title)
+        self.la_body_all.addItem(spacerItem2)
+        self.la_body_all.setObjectName("la_body_all")
+        #layout - content
+        self.la_footer_body = QtWidgets.QVBoxLayout()
+        self.la_footer_body.addWidget(self.l_info)
+        self.la_footer_body.addWidget(self.input_info)
+        self.la_footer_body.addWidget(self.btn_ok)
+        self.la_footer_body.addItem(spacerItem4)
+        self.la_footer_body.setObjectName("la_footer_body")
+        #layout - body
+        self.la_footer_all = QtWidgets.QHBoxLayout()
+        self.la_footer_all.addItem(spacerItem3)
+        self.la_footer_all.addLayout(self.la_footer_body)
+        self.la_footer_all.addItem(spacerItem5)
+        self.la_footer_all.setObjectName("la_footer_all")
+        #layout - html
+        self.la_content = QtWidgets.QVBoxLayout()
+        self.la_content.addLayout(self.la_body_all)
+        self.la_content.addLayout(self.la_footer_all)
+        self.la_content.setObjectName("la_content")
+        
+        
+        self.verticalLayout_3.addLayout(self.la_header)
+        self.verticalLayout_3.addLayout(self.la_content)
+        self.setCentralWidget(self.w_main)
+        self.retranslateUi(self)
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    # function
+    def shutdown(self):
+        self.close()
+        sys.exit(0)
+
+    def checkInput(self, to):
+        if self.input_info.text():
+            self.btn_ok.setEnabled(True)
+        else:
+            self.btn_ok.setEnabled(False)
+
+    def mousePressEvent(self, event):
+        self.old_pos = event.globalPos()
+        self.mouse_down = event.button()== Qt.LeftButton
+
+    def mouseReleaseEvent(self, e):
+        self.mouse_down = False
+
+    def mouseMoveEvent(self, event):
+        x=event.x()
+        y=event.y()
+        self.move(self.pos() + event.globalPos() - self.old_pos)
+        self.old_pos = event.globalPos()
+        event.accept()
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        self.label_2.setText(_translate("MainWindow", "SyncN"))
-        self.label.setText(_translate("MainWindow", "Typing Your E-mail"))
-        self.lineEdit.setPlaceholderText(_translate("MainWindow", "exampel@mail.com"))
-        self.pushButton.setText(_translate("MainWindow", "OK"))
+        self.w_main.setToolTip(_translate("MainWindow", "Sync your sticky note on Windows 10"))
+        self.btn_tray.setToolTip(_translate("MainWindow", "Minimumrize window(Tray)"))
+        self.btn_tray.setText(_translate("MainWindow", "_"))
+        self.btn_close.setToolTip(_translate("MainWindow", "Close"))
+        self.btn_close.setText(_translate("MainWindow", "X"))
+        self.l_title.setText(_translate("MainWindow", "SyncN"))
+        self.l_info.setText(_translate("MainWindow", "Typing Your E-mail"))
+        self.input_info.setToolTip(_translate("MainWindow", "Using only your E-mail"))
+        self.input_info.setPlaceholderText(_translate("MainWindow", "exampel@mail.com"))
+        self.btn_ok.setToolTip(_translate("MainWindow", "Click when you sure Okay"))
+        self.btn_ok.setText(_translate("MainWindow", "OK"))
 
+    
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
+    MainWindow = UI()
     MainWindow.show()
     sys.exit(app.exec_())
-
