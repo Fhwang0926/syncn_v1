ce83e87e5cd0b83eb50e290b615a055b433e9d0b
root <hdh0926@naver.com>
2018-10-07 19:51:41 +0900

    a

--- Agent/build.cmd
+++ Agent/build.cmd
@@ -4,7 +4,7 @@ rmdir /s /q dist
 rmdir /s /q build
 del *.spec
 xcopy Lib C:\Users\hdh09\AppData\Local\Programs\Python\Python36\Lib\Lib /E /Y
-pyinstaller --onefile --nowindowed --hidden-import="C:\Users\hdh09\AppData\Local\Programs\Python\Python36\Lib\site-packages\PyQt5\sip.pyd" --icon="E:\Project\Agent\images\sync.ico" --clean -p ./build_windows SyncN.py
+pyinstaller --onefile --windowed --hidden-import="C:\Users\hdh09\AppData\Local\Programs\Python\Python36\Lib\site-packages\PyQt5\sip.pyd" --icon="E:\Project\Agent\images\sync.ico" --clean -p ./build_windows SyncN.py
 copy init.syncn dist\setting.syncn
 xcopy images dist\images  /E /Y
 echo on
